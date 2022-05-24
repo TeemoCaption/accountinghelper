412f19a67bf22076651f9c6961afa33542bb1ed1
teemocaption <eyan0816@gmail.com>
2022-05-24 11:09:02 +0800

    ok

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text=msg)
+        message = TextSendMessage(text=msg)     #說一樣的話
         line_bot_api.reply_message(event.reply_token, message)
 
 @handler.add(PostbackEvent)
