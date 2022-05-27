1f1ecb8c4bb6db062eaa909f780bb69dc617ca07
TeemoCaption <eyan0816@gmail.com>
2022-05-27 14:43:13 +0800

    ok

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def button_reply():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg=button_reply()
+    msg = event.message.text
     if '最新合作廠商'  == msg:
         message = imagemap_message()
         line_bot_api.reply_message(event.reply_token, message)
