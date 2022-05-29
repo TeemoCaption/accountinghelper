ce5c64bb46f0dcdfbbfb9b17424d176392c8040b
TeemoCaption <eyan0816@gmail.com>
2022-05-29 21:47:25 +0800

    ok

--- app.py
+++ app.py
@@ -32,11 +32,8 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi('Tnn7ruaJTFJSF065VRDLe7T5DqGpzXLKHlKdISIRzr3A1qyjB7UvgPve40QMHmWlPvDvvXFuoeyodR6wmn6fwIciyBL7uBDAsd2NjdjbuLVFSRO2oDjms4imFs8jz+PShjzYojdlWOd0eL8Z9SMyEAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('a8ce48921e34d218c60bcbaf3cca1861')
-
-
 liff_api = LIFF("Tnn7ruaJTFJSF065VRDLe7T5DqGpzXLKHlKdISIRzr3A1qyjB7UvgPve40QMHmWlPvDvvXFuoeyodR6wmn6fwIciyBL7uBDAsd2NjdjbuLVFSRO2oDjms4imFs8jz+PShjzYojdlWOd0eL8Z9SMyEAdB04t89/1O/w1cDnyilFU=")
-line_bot_api = LineBotApi("Tnn7ruaJTFJSF065VRDLe7T5DqGpzXLKHlKdISIRzr3A1qyjB7UvgPve40QMHmWlPvDvvXFuoeyodR6wmn6fwIciyBL7uBDAsd2NjdjbuLVFSRO2oDjms4imFs8jz+PShjzYojdlWOd0eL8Z9SMyEAdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("bb44176631709b8991e0fb427424a8b2")
+
 
 try:
     now_LIFF_APP_number = len(liff_api.get())
