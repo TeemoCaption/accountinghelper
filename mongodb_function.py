d5cca59858b4b9c835dc921cab6d43dd0a0faecb
TeemoCaption <eyan0816@gmail.com>
2022-06-07 14:16:08 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -69,7 +69,7 @@ def read_date(date):
         dataNo="n"+str(i)
         data_list[dataNo]['class']=data['class']
         i+=1
-    return str(data_list)
+    return "123"
 
 #讀取LINE的對話紀錄資料
 def read_chat_records():
