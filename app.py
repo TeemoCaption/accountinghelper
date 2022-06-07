edd5d23841d9706ce94d6b746a5ae042b5251b86
TeemoCaption <eyan0816@gmail.com>
2022-06-07 16:17:38 +0800

    ok

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def handle_message(event):
     global user_id
     global event_token
     user_id=event.source.user_id
-    event_token=event.replyToken
+    event_token=event.reply_Token
     if '查看功能' ==msg:
         message = button_reply()
         line_bot_api.reply_message(event.reply_token, message)
