3a4b1f62819d1da715c9056a17ee1739d7a0ab71
TeemoCaption <eyan0816@gmail.com>
2022-06-10 10:21:22 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -69,8 +69,7 @@ def read_date(user,date):
     data_list=[]
     n=0
     for data in col.find({'user_id': user,'date':{'$regex':target_date}}):   # $regex正規表達式
-        data_list.append([])
-        data_list[n].append(data['class'],data['type'],data['item'],data['money'],data['keep'])
+        data_list[n].append([data['class'],data['type'],data['item'],data['money'],data['keep']])
         n+=1
         
     return data_list
