fe1dc10751477d3db43c7b10866e84672fb4a65f
TeemoCaption <eyan0816@gmail.com>
2022-06-10 09:52:41 +0800

    ok

--- edit_message.py
+++ edit_message.py
@@ -69,145 +69,143 @@ def find_date(user,date):
     i=0
     for data in data_list:
         bubble={
-            {
-                "type": "bubble",
-                "hero": {
-                    "type": "image",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover",
-                    "action": {
-                    "type": "uri",
-                    "uri": "http://linecorp.com/"
-                    }
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                "type": "uri",
+                "uri": "http://linecorp.com/"
+                }
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                {
+                    "type": "text",
+                    "text": data[i][0],
+                    "weight": "bold",
+                    "size": "xl"
                 },
-                "body": {
+                {
                     "type": "box",
                     "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
                     "contents": [
                     {
-                        "type": "text",
-                        "text": data[i][0],
-                        "weight": "bold",
-                        "size": "xl"
-                    },
-                    {
                         "type": "box",
-                        "layout": "vertical",
-                        "margin": "lg",
+                        "layout": "baseline",
                         "spacing": "sm",
                         "contents": [
                         {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "類別：",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": data[i][1],
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
+                            "type": "text",
+                            "text": "類別：",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
                         },
                         {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "項目：",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": data[i][2],
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
+                            "type": "text",
+                            "text": data[i][1],
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 5
+                        }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "項目：",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
                         },
                         {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "金額：",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": data[i][3],
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
+                            "type": "text",
+                            "text": data[i][2],
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 5
+                        }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "金額：",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
                         },
                         {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                            {
-                                "type": "text",
-                                "text": "備註：",
-                                "color": "#aaaaaa",
-                                "size": "sm",
-                                "flex": 1
-                            },
-                            {
-                                "type": "text",
-                                "text": data[i][4],
-                                "wrap": True,
-                                "color": "#666666",
-                                "size": "sm",
-                                "flex": 5
-                            }
-                            ]
+                            "type": "text",
+                            "text": data[i][3],
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 5
+                        }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "備註：",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                        },
+                        {
+                            "type": "text",
+                            "text": data[i][4],
+                            "wrap": True,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 5
                         }
                         ]
                     }
                     ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                        "type": "uri",
-                        "label": "修改該筆紀錄",
-                        "uri": "https://linecorp.com"
-                        }
-                    }
-                    ],
-                    "flex": 0
                 }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                    "type": "uri",
+                    "label": "修改該筆紀錄",
+                    "uri": "https://linecorp.com"
+                    }
+                }
+                ],
+                "flex": 0
             }
         }
     bubbles.append(bubble)
