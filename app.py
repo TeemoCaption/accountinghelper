54c55a3fbc074e88ebf24231b8f23f85bf7a2166
TeemoCaption <eyan0816@gmail.com>
2022-06-12 17:58:48 +0800

    ok

--- app.py
+++ app.py
@@ -130,9 +130,8 @@ def get_dateData(event):
     data=event.postback.data
     date=event.postback.params['date']
     user=event.source.user_id
-    message=[]
     if data=="editdate":
-        message.append(find_date(user,date))
+        message=find_date(user,date)
         line_bot_api.reply_message(event.reply_token, message)
     
     
