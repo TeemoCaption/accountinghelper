b282c439da3d2847716a0d85cc5f0a4600f0ebf1
TeemoCaption <eyan0816@gmail.com>
2022-06-13 23:58:26 +0800

    ok

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def index():
     return render_template("./liff.html")
 
 
-@app.route("/edit_data/<int:num>",methods=["GET","POST"])
+@app.route("/edit_data/<num>",methods=["GET","POST"])
 def edit_html(num):
     n=int(num)
     edit_data=edit_list[n]
