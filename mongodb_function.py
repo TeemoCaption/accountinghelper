494c3553ac9a8b5cf2afc3c5245897b6a845baea
TeemoCaption <eyan0816@gmail.com>
2022-06-16 01:09:57 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -69,7 +69,7 @@ def read_date(user,date):
     null_str=""
     for data in col.find({'user_id': user,'date':{'$regex':target_date}}):  # $regex正規表達式
         if(str(data.get('keep'))==''):
-            null_str="無"
+            null_str='無'
         data_list.append([str(data.get('class')),str(data.get('type')),str(data.get('item')),str(data.get('money')),null_str])
     return data_list
 
