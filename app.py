64cb851f4237bb982b806e25e0c1060831d8a49d
TeemoCaption <eyan0816@gmail.com>
2022-06-17 15:54:39 +0800

    ok

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def edit_html(num):
         item=request.form.get('item')
         money=request.form.get('money')
         keep=request.form.get('keep')
-        updateData(edit_data[0], m_class, date, m_type, item, money, keep)
+        updateData(user_id, m_class, date, m_type, item, money, keep)
         message="紀錄更新完成"
         line_bot_api.push_message(user_id,TextSendMessage(text=message))
     return render_template('./edit_data.html',data=edit_data)
