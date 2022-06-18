0373579c40825ca53cb9dc52e14f09d322c25485
TeemoCaption <eyan0816@gmail.com>
2022-06-18 17:38:27 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -108,5 +108,5 @@ def updateData(user_id,m_class,date,m_type,item,money,keep):
     find={"user_id":user_id,"date":date}
     date=str(date).replace('T',' ')
     money=int(money)
-    post={"$set":{"user_id": user_id,"class":m_class,"date":date,"type":m_type,"item":item,"money":int(money),"keep":keep}}
+    post={"$set":{"user_id": user_id,"class":m_class,"date":date,"type":m_type,"item":item,"money":money,"keep":keep}}
     col.update_one(find, post)
