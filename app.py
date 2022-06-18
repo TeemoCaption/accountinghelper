9f3448dc83228eb58fb60c708ddb616d88b21b81
TeemoCaption <eyan0816@gmail.com>
2022-06-18 17:59:55 +0800

    ok

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def edit_html(num):
         updateData(edit_data[0], m_class, date, m_type, item, money, keep)
         message="紀錄更新完成"
         line_bot_api.push_message(edit_data[0],TextSendMessage(text=message))
-    return render_template('./edit_data.html',data=edit_data,num=num)
+    return render_template('./edit_data/<num>',data=edit_data,num=num)
 
     
 # 監聽所有來自 /callback 的 Post Request
