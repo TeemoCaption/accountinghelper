f41937f0e8fae82310b3b18b46466d27a6bc30c2
TeemoCaption <eyan0816@gmail.com>
2022-06-18 17:19:20 +0800

    ok

--- edit_message.py
+++ edit_message.py
@@ -87,7 +87,7 @@ def find_date(user,date):
                 "contents": [
                 {
                     "type": "text",
-                    "text": data[i][0],
+                    "text": data[i][1],
                     "weight": "bold",
                     "size": "xl"
                 },
@@ -111,7 +111,7 @@ def find_date(user,date):
                         },
                         {
                             "type": "text",
-                            "text": data[i][1],
+                            "text": data[i][2],
                             "wrap": True,
                             "color": "#666666",
                             "size": "sm",
@@ -133,7 +133,7 @@ def find_date(user,date):
                         },
                         {
                             "type": "text",
-                            "text": data[i][2],
+                            "text": data[i][3],
                             "wrap": True,
                             "color": "#666666",
                             "size": "sm",
@@ -155,7 +155,7 @@ def find_date(user,date):
                         },
                         {
                             "type": "text",
-                            "text": data[i][3],
+                            "text": data[i][4],
                             "wrap": True,
                             "color": "#666666",
                             "size": "sm",
@@ -177,7 +177,7 @@ def find_date(user,date):
                         },
                         {
                             "type": "text",
-                            "text": data[i][4],
+                            "text": data[i][5],
                             "wrap": True,
                             "color": "#666666",
                             "size": "sm",
