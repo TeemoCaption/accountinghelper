41707ec17906f4eca700263dd14dd7e363d2e1e8
TeemoCaption <eyan0816@gmail.com>
2022-06-19 23:55:26 +0800

    ok

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def index():
 
 @app.route("/edit_data/<int:num>/",methods=["GET","POST"])
 def edit_data(num):
-    user_id=col.find_one({"rid":num})
+    user_id=col.find_one({"rid":num}).get('user_id')
     edit_data=edit_list[num]
     if request.method=="POST":
         m_class=request.form.get('class')
