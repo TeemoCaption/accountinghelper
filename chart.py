9903dbe77c79fd86376268501fd0839c9fc73ae0
TeemoCaption <eyan0816@gmail.com>
2022-06-21 22:25:48 +0800

    ok

--- chart.py
+++ chart.py
@@ -29,11 +29,8 @@ def show_income(user_id):
         
     plt.figure(figsize=(6,9))
     color=["#ef233c","#219ebc","#fca311","#2ec4b6","#fcbc00","#ef9cda","#b298dc","#f4d35e","#00c49a","#9381ff","#edf67d"]
-    plt.pie(money,explode=None,labels=type_list,colors=color,labeldistance=1.1,autopct = "%1.2f%%",shadow=True,startangle=90,pctdistance=0.6,textprops = {"fontsize" : 12})
-    plt.axis('equal') 
+    plt.pie(money,explode=None,labels=type_list,colors=color,labeldistance=1.1,autopct = "%1.2f%%",shadow=True,startangle=90,pctdistance=0.6)
     plt.title("本月收入", {"fontsize" : 18})
-    plt.legend()   
-    
     file_path="/images/"+str(user_id)+"_1.jpg"
     plt.savefig(file_path,bbox_inches='tight',pad_inches=0.0)     
     plt.close()
