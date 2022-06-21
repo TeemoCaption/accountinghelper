20b28ab43721886ae0ea7c588fedd2cf5ac10612
TeemoCaption <eyan0816@gmail.com>
2022-06-21 18:35:26 +0800

    ok

--- mongodb_function.py
+++ mongodb_function.py
@@ -113,8 +113,8 @@ def updateData(rid,m_class,date,m_type,item,money,keep):
 
 def find_income(user_id):
     today=datetime.date.today()
-    date="/.*"+str(today)[:7]+".*/"
+    date=str(today)[:7]
     data_list=[]
-    for data in col.find({'user_id':user_id,"date":{'$regex':date},"class":"收入"}):
+    for data in col.find({'user_id':user_id,"date":{'$regex':f".*{date}.*"},"class":"收入"}):
         data_list.append([str(data.get('type')),int(data.get('money')),str(data.get('user_id'))])
     return data_list
